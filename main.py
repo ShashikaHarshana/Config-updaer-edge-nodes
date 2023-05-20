8fecca445f8ad43a1d869ec9b1c0a0de7faad2e5
Shashika Harshana <shashika.18@cse.mrt.ac.lk>
2023-05-20 19:16:30 +0530

    flask server integration

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 # This is the config updater which runs in the edge nodes.
 import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/config_update')
+def handle_config_update():
+    update_req = request.args.get('update_req')
+    print(update_req)
+    res = update_config(update_req)
+    print(res)
+    return res
+
+
+@app.route('/')
+def form_example():
+    return 'hello from config updater server'
+
 
 esp32_url = 'http://192.168.47.235/'
+
+
+def update_config(update_req):
+    req_url = esp32_url + 'control'
+    get_req_framesize = get_request_payload(update_req)
+    params = {'quality': get_req_framesize}
+
+    response = requests.get(req_url, params=params)
+
+    if response.status_code == 200:
+        print(response.text)
+        return response.text
+    else:
+        print('config update failed with status code: ', response.status_code)
 
 
 def get_curr_frame_size():
     config_url = esp32_url + 'config'
 
-    response = requests.get(config_url)
+    res = requests.get(config_url)
 
-    if response.status_code == 200:
-        curr_frame_size = response.text
+    if res.status_code == 200:
+        curr_frame_size = res.text
 
     else:
-        print('Request failed with status code:', response.status_code)
+        print('Request failed with status code:', res.status_code)
 
     return curr_frame_size
 
@@ -55,40 +87,46 @@ def get_request_payload(quality_measure):
         return req_frame_size
 
 
-def print_hi(name):
-    # Define the ESP32 web server URL
-
-    # Define the event or condition that triggers the HTTP request
-    event_occurred = True
-
-    if event_occurred:
-        # Create a payload if required
-        payload = {'key1': 'value1', 'key2': 'value2'}
-
-        # Send an HTTP GET or POST request to the ESP32 web server
-        response = requests.post(esp32_url, data=payload)
-
-        # Check the response status code
-        if response.status_code == 200:
-            print('HTTP request sent successfully')
-        else:
-            print('Failed to send HTTP request')
+# def print_hi(name):
+#     # Define the ESP32 web server URL
+#
+#     # Define the event or condition that triggers the HTTP request
+#     event_occurred = True
+#
+#     if event_occurred:
+#         # Create a payload if required
+#         payload = {'key1': 'value1', 'key2': 'value2'}
+#
+#         # Send an HTTP GET or POST request to the ESP32 web server
+#         response = requests.post(esp32_url, data=payload)
+#
+#         # Check the response status code
+#         if response.status_code == 200:
+#             print('HTTP request sent successfully')
+#         else:
+#             print('Failed to send HTTP request')
+#
+#     def update_config(update_req):
+#         req_url = esp32_url + 'control'
+#
+#         get_req_framesize = get_request_payload(update_req)
+#         params = {'quality': get_req_framesize}
+#
+#         response = requests.get(req_url, params=params)
+#
+#         if response.status_code == 200:
+#             print(response.text)
+#         else:
+#             print('config update failed with status code: ', response.status_code)
+#
+#         if response.status_code == 200:
+#             print(response.text)
+#         else:
+#             print('config update failed with status code: ', response.status_code)
 
 
 def main():
-    # Listen to kubernetes streams and call the specified url with payload.
-    to_do = 'decrease'
-    req_url = esp32_url + 'control'
-    if 1 == 1:
-        get_req_framesize = get_request_payload(to_do)
-        params = {'quality': get_req_framesize}
-
-        response = requests.get(req_url, params=params)
-
-        if response.status_code == 200:
-            print(response.text)
-        else:
-            print('config update failed with status code: ', response.status_code)
+    app.run(host='0.0.0.0', port=5000)
 
 
 # Press the green button in the gutter to run the script.
